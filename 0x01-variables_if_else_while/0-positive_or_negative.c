2fdd135ad3f81417886b0c03edf6f02133115272
Duncan-pk <pkduncan34@gmail.com>
2022-10-17 03:00:26 -0700

    first if statement

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+
+		int n;
+
+		srand(time(0));
+
+		n = rand() - RAND_MAX / 2;
+
+		/* your code goes there */
+		if (n > 0)
+		{
+		printf("%d", n);
+		puts(" is positive");
+		}
+		else if (n == 0)
+		{
+		printf("%d", n);
+		puts(" is zero");
+		}
+		else
+		{
+		printf("%d", n);
+		puts("is negative");
+		}
+		return (0);
+
+}
