f6f908f8b151c395d6b876cf121b40cab5adf752
Duncan-pk <pkduncan34@gmail.com>
2022-10-18 12:28:36 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,26 +2,10 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * main -Entry point
+ * _islower -Entry point
  *
  * Return: always return 0 (Success)
  */
-int main(void)
-{
-	int r;
-	r = _islower('H');
-	r = _islower('o');
-	r = _islower(108);
-	_putchar('\n');
-	return (0);
-}
-
-int _putchar(char c)
-{
-
-	return (write(1, &c, 1));
-}
-
 int _islower(int c)
 {
 	if (islower(c) > 0)
