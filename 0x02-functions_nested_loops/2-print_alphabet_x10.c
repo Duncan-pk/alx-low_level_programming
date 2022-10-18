98084231065e687bf977869a21fae98eac58824d
Duncan-pk <pkduncan34@gmail.com>
2022-10-18 12:21:51 -0700

    task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,9 @@
-#include <unistd.h>
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet_x10 - Entry point
  *
  * Return: always return 0 (Success)
  */
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
-}
-
-int _putchar(char c)
-{
-
-	return (write(1, &c, 1));
-}
-
-
 void print_alphabet_x10(void)
 {
 	char c;
