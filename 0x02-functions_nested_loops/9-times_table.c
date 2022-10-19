189f801feced7713120203e48cef29a500749169
Duncan-pk <pkduncan34@gmail.com>
2022-10-19 06:41:16 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,18 @@
 void times_table(void)
 {
 int m, x, z;
+char display[];
 
 for (m = 0 ; m < 10 ; m++)
 {
 	for (x = 0 ; x < 10 ; x++)
 	{
 	z = x * m;
-	_putchar((z % 10) + '0');
+	display[x] = 'z';
+	for (int a = 0 ; a <= 1 ; a++)
+	{
+	_putchar(display[a]);
+	}
 	_putchar(',');
 	_putchar(' ');
 	}
