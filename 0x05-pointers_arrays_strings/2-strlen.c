5efd1eefc6f5c5c5f4cfc11315e957a2af8d6f78
Duncan-pk <pkduncan34@gmail.com>
2022-10-24 02:43:04 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s: pointer
+ * Return: return the length
+ */
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+return (length);
+}
