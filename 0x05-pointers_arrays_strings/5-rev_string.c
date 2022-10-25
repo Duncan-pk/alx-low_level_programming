e7f909dc7c4a82977e0eb369ca3d172b6af0ea5f
Duncan-pk <pkduncan34@gmail.com>
2022-10-25 12:08:49 -0700

    second try

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,9 +9,8 @@ void rev_string(char *s)
 	int c = 0;
 	int i;
 
-	while (s[c] != '\0')
-	{
-		c++;
+		while (s[c] != '\0')
+			c++;
 		for (i = 0; i < c; i++)
 		{
 			c--;
@@ -19,5 +18,5 @@ void rev_string(char *s)
 			s[i] = s[c];
 			s[c] = rv;
 		}
-	}
+	
 }
