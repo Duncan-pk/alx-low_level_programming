8d93e4b845345f4e149f5b96cbf76a3964d1269a
Duncan-pk <pkduncan34@gmail.com>
2022-10-31 10:40:27 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy - function
+ * @dest: destination
+ * @src: source
+ * @n: array controler
+ * Return: return char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+
+	for ( i = 0; n > 0; i++,n--)
+	{
+		dest[i] = src[i];
+	}
+return (dest);
+}
