371a408b99ab4107ea4e22c156cf190b12d9216b
Duncan-pk <pkduncan34@gmail.com>
2022-10-31 06:59:49 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - Function
+ * @s: buffer
+ * @b: copy item
+ * @n: array controler
+ * Return: char
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	int x;
+	x = (int)n;
+	/*loop set for only n characters*/
+
+	for (i = 0; i < x; i++)
+	{
+	s[i] = b;
+	}
+
+	/*output the entire loop*/
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+THIS IS A README FOR 0x07-pointers_arrays_strings
